82f63675a1ef7c721fb578af54f6877996e2d6eb
Hojjat Hekmatipour <158481246+Hojjat-hk@users.noreply.github.com>
2024-02-12 21:23:27 +0330

    Update Main.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -18,7 +18,9 @@ function focusOnInputHandler(item){
     item.target.style.cssText = "position: static;padding: 0;font-size:12px;";
     item.target.parentElement.style.cssText = "background-color: #fff;border-color:#0671E0";
     item.target.nextElementSibling.style.cssText = "visibility: visible;";
-    item.target.parentElement.lastElementChild.style.visibility = "visible";
+    if(item.target.parentElement.lastElementChild){
+        item.target.parentElement.lastElementChild.style.visibility = "visible";
+    }
 }
 function temporaryValidation(event){
     let inputValue = event.value;
