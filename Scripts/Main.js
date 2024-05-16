cdfba8c76108aa6ef686eacfb1eede47826665a2
Hojjat Hekmatipour <158481246+Hojjat-hk@users.noreply.github.com>
2024-05-16 11:04:26 +0330

    Update Main.js

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -11,7 +11,8 @@ const showPasswordBtn = $.querySelectorAll(".passwordEye");
 // [+] Regular expression patterns
 const nameValidateRegEx = /^[A-Za-zÀ-ÖØ-öø-ÿءآ-ی ‌]{2,30}$/;
 const emailValidateRegEx = /^[A-Za-z0-9.+\-_~!#$%&‘'/=^{}|*?`]+@[A-Za-z0-9][A-Za-z0-9-]*(?:\.[A-Za-z0-9-]+)+[A-Za-z0-9]$/;
-const passwordValidateRegEx = /^.{8,16}$/
+const passwordValidateRegEx = /^.{8,16}$/;
+
 // [+] Functions
 function focusOnInputHandler(item){
     this.classList.contains("input-filed--active") || this.classList.add("input-filed--active");
@@ -24,6 +25,7 @@ function focusOnInputHandler(item){
         }
     },50)
 }
+
 function temporaryValidation(event){
     let inputValue = event.value;
     if(!inputValue || inputValue.length < 2){
@@ -32,16 +34,20 @@ function temporaryValidation(event){
         PUValidInput(event);
     }
 }
+
 function invalidInput(element){
     element.parentElement.classList.add("invalid-input");
 }
+
 function validInput(element){
     element.parentElement.classList.add("valid-input");
 }
+
 function PUValidInput(element){
     element.parentElement.classList.contains("invalid-input") && element.parentElement.classList.remove("invalid-input");
     element.parentElement.classList.contains("valid-input") && element.parentElement.classList.remove("valid-input");
 }
+
 function inputValidate(inputElem, datasetValue, regEx, modalStatus, modalValue){
     let inputValue = inputElem.value.trim();
     let variable = "";
@@ -57,6 +63,7 @@ function inputValidate(inputElem, datasetValue, regEx, modalStatus, modalValue){
     }
     return variable;
 }
+
 function showModal(modalStatus, modalValue){
     if(modalStatus){
         modalElem.style.cssText = `background-color:#74E291;transform: translateX(0);`
@@ -72,6 +79,7 @@ function closeModal(){
         modalElem.style.transform = "translateX(100%)";
     },2800);
 }
+
 function showPasswordHandler(event){
     if(event.target.classList.contains("fa-eye-slash")){
         event.target.classList.replace("fa-eye-slash", "fa-eye");
@@ -81,29 +89,30 @@ function showPasswordHandler(event){
         event.target.previousElementSibling.setAttribute("type", "password");
     }
 }
+
 function changeLocation (locationX = ""){
     setTimeout(function (){
         location.replace(locationX);
-    },1000)
+    },1000);
 }
+
+function preventDefaultHandler(event){
+    event.preventDefault();
+}
+
 // [+] Events
 inputFiled.forEach(function(item){
-    item.addEventListener("click", focusOnInputHandler)
+    item.addEventListener("click", focusOnInputHandler);
 });
 showPasswordBtn.forEach(function (btn){
-    btn.addEventListener("click", showPasswordHandler)
+    btn.addEventListener("click", showPasswordHandler);
 })
 inputElems.forEach(function(item){
-    item.addEventListener("blur", function(){
-        temporaryValidation(item)
+    item.addEventListener("blur", () => {
+        temporaryValidation(item);
     });
-    item.addEventListener("copy", function (event){
-        event.preventDefault();
-    });
-    item.addEventListener("cut", function (event){
-        event.preventDefault();
-    });
-    item.addEventListener("paste", function (event){
-        event.preventDefault();
-    });
+    // Disabled Copy - Cut - Paste on input 
+    item.addEventListener("copy", preventDefaultHandler);
+    item.addEventListener("cut", preventDefaultHandler);
+    item.addEventListener("paste", preventDefaultHandler);
 });
